f891416a8b3e37e3c8245de65c0ebc69cc363225
Md. Shamim Miah <shamim.miah@brainstation-23.com>
2024-12-20 01:11:32 +0600

    add cookie

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use Cookie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -28,16 +29,26 @@ class AuthController extends Controller
             $user = Auth::user();
             $token = $user->createToken('admin')->accessToken;
             // dd($user);
+            $cookie = cookie('jwt', $token, 3600);
 
-            return [
+            return response([
                 'status' => true,
                 'message' => "Token genrate Successfully",
                 'token' => $token,
-            ];
+            ])->withCookie($cookie);
         }
 
         return response([
             'error' => 'Invalid Credentials!',
         ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+        return response([
+            'status' => true,
+            'message' => "Success logout",
+        ])->withCookie($cookie);
     }
 }
