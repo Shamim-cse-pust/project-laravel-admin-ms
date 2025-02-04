5c887f7ec4a203d0c03249c4f0d09aa9e9134d87
Md. Shamim Miah <shamim.miah@brainstation-23.com>
2025-02-05 00:39:00 +0600

    add Chart for showing orders

--- routes/api.php
+++ routes/api.php
@@ -5,22 +5,24 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProductController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/permissions', [PermissionController::class, 'index']);
 Route::group(
     ['middleware' => 'auth:api'],
     function () {
+        Route::get('/chart', [DashboardController::class, 'chart']);
         Route::apiResource('users', UserController::class);
         Route::apiResource('roles', RoleController::class);
         Route::apiResource('/products', ProductController::class);
         Route::get('/orders/export', [OrderController::class, 'export']);
         Route::Resource('orders', OrderController::class)->only(['index','show']);
         Route::apiResource('users', UserController::class);
+        Route::get('/permissions', [PermissionController::class, 'index']);
         Route::group(
             ['prefix' => '/profile'],
             function () {
